22f74643fe5468873dd4bf1148289c09b265fbde
TomStuart92 <Thomascstuart@gmail.com>
2016-08-28 12:08:43 +0100

    WIP - Set1 Ex6

--- Set1/6.breakrepeatkey.rb
+++ Set1/6.breakrepeatkey.rb
@@ -1,32 +1,38 @@
 require 'base64'
 
 string = []
-
-File.open("repeating_key.txt", "r") do |f|
+File.open("6a.repeating_key.txt", "r") do |f|
   f.each_line do |line|
     string << line.chomp
   end
-  string = Base64.strict_decode64(string.join)
 end
 
 keysizes = [*2..40]
-
+binary_string = Base64.strict_decode64(string.join).unpack('B*')[0]
 
 def hamming_distance(binary1, binary2)
   binary1.chars.zip(binary2.chars).map{|a,b| a.ord == b.ord ? 0 : 1}.inject(:+)
 end
 
-def select_n_bytes_package(n, string)
-  string.unpack('B*')[0].scan(/.{#{n}}/)
+def test_hamming_distance
+  hamming_distance('wokka wokka!!!'.unpack('B*')[0], 'this is a test'.unpack('B*')[0]) == 37
+end
+
+def select_bytes_package(n, binary_string)
+  x = binary_string.scan(/......../)
+  [x[0..n-1].join, x[n..2*n].join]
 end
 
 def check_key_size(keysizes, string)
   output = []
   keysizes.each do |keysize|
-    bytes = select_n_bytes_package(keysize, string)
-    output << [keysize, hamming_distance(bytes[0], bytes[1]).to_f / keysize]
+    bytes = select_bytes_package(keysize, string)
+    output << [keysize, (hamming_distance(bytes[0],bytes[1])).to_f / keysize]
   end
- output.sort_by {|k, h| h}[0..3]
+
+ output.sort_by {|k, h| h}
 end
+
+p check_key_size(keysizes, binary_string)
 
 #[[40, 0.15], [24, 0.25], [10, 0.3], [16, 0.3125]]
